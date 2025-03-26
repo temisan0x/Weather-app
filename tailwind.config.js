7ef3735258209e15d240679ce1d53209e2a8a737
temycodes <temycodes@gmail.com>
2025-03-26 17:04:26 +0100

    feat: Initial setup: Added Tailwind CSS, HTML, and JS structure

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js}"], // Correct glob pattern
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
